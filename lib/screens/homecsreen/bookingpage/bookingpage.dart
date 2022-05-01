594eec644cafb483159f7ed467cf4fb096757772
Nguyễn Đức Hoàng Phi <nguyenduchoangphi922016@gmail.com>
2022-04-30 20:13:46 -0700

    improve booking_page ui

--- lib/screens/homecsreen/bookingpage/bookingpage.dart
+++ lib/screens/homecsreen/bookingpage/bookingpage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:home_services/config/fonts.dart';
 import 'package:home_services/config/theme.dart';
+import 'task_page.dart';
 
 class BookingPage extends StatefulWidget {
   const BookingPage({Key? key}) : super(key: key);
@@ -11,111 +12,108 @@ class BookingPage extends StatefulWidget {
 
 class _BookingPageState extends State<BookingPage> {
   int _selectIndex = 0;
+  bool add = false;
   void _onItemTapped(int index) {
     setState(() {
       _selectIndex = index;
     });
   }
 
-  final List<Widget> _widgetOptions = <Widget>[
-    const Text('dhds'),
-    const BookingPage(),
-    const Text('Admin')
-  ];
+  void _onAddTask() {
+    setState(() {
+      add = !add;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        // crossAxisAlignment: CrossAxisAlignment.stretch,
-        // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      appBar: AppBar(backgroundColor: Colors.white, actions: [
+        Container(
+          width: MediaQuery.of(context).size.width / 3,
+          decoration: BoxDecoration(
+              border: Border(
+                  bottom: BorderSide(
+                      color: _selectIndex == 0
+                          ? ColorApp.purpleColor
+                          : ColorApp.textColor2,
+                      width: 4))),
+          child: TextButton(
+              style: TextButton.styleFrom(backgroundColor: Colors.white),
+              onPressed: () {
+                _onItemTapped(0);
+                // Navigator.push(
+                //     context,
+                //     MaterialPageRoute(
+                //         builder: (context) => const LoginScreen()));
+              },
+              child: Text('Hiện tại', style: FontStyle().lineFont)),
+        ),
+        Container(
+          width: MediaQuery.of(context).size.width / 3,
+          decoration: BoxDecoration(
+              border: Border(
+                  bottom: BorderSide(
+                      color: _selectIndex == 1
+                          ? ColorApp.purpleColor
+                          : ColorApp.textColor2,
+                      width: 4))),
+          child: TextButton(
+              style: TextButton.styleFrom(backgroundColor: Colors.white),
+              onPressed: () {
+                _onItemTapped(1);
+              },
+              child: Text('Đăng việc', style: FontStyle().lineFont)),
+        ),
+        Container(
+          width: MediaQuery.of(context).size.width / 3,
+          decoration: BoxDecoration(
+              border: Border(
+                  bottom: BorderSide(
+                      color: _selectIndex == 2
+                          ? ColorApp.purpleColor
+                          : ColorApp.textColor2,
+                      width: 4))),
+          child: TextButton(
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.white,
+              ),
+              onPressed: () {
+                _onItemTapped(2);
+              },
+              child: Text('Lịch sử', style: FontStyle().lineFont)),
+        ),
+      ]),
+      body: add == false ? emptyTask() : TaskPage(),
+    );
+  }
+
+  Center emptyTask() {
+    return Center(
+      // height: double.infinity,
+
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        // crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Container(
-            height: 50,
-            child: Row(
-              children: [
-                Container(
-                  width: MediaQuery.of(context).size.width / 3,
-                  child: Center(
-                    child: TextButton(
-                      onPressed:(){
-                        _onItemTapped(0);
-                      } ,
-                      child: Text('Text Button'),
-                    ),
-                  ),
-                  decoration: const BoxDecoration(
-                      border: Border(
-                    bottom: BorderSide(
-                      color: ColorApp.purpleColor,
-                      width: 4,
-                    ),
-                  )),
-                ),
-                Container(
-                  width: MediaQuery.of(context).size.width / 3,
-                  // padding: const EdgeInsets.all(16),
-                  child: const Center(
-                      child: Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 16),
-                    child: Text('dang viec'),
-                  )),
-                  decoration: const BoxDecoration(
-                      border: Border(
-                    bottom: BorderSide(
-                      color: ColorApp.purpleColor,
-                      width: 4,
-                    ),
-                  )),
-                ),
-                Container(
-                  width: MediaQuery.of(context).size.width / 3,
-                  // padding: const EdgeInsets.all(16),
-                  child: const Center(
-                    child: Text('dang y viec'),
-                  ),
-                  decoration: const BoxDecoration(
-                      border: Border(
-                    bottom: BorderSide(
-                      color: ColorApp.purpleColor,
-                      width: 4,
-                    ),
-                  )),
-                ),
-              ],
-            ),
+          Image.asset('assets/images/logodemo.png'),
+          const SizedBox(
+            height: 32,
           ),
-          // const Spacer(),
-          Expanded(
-              child: Center(
-            // height: double.infinity,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              // crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Image.asset('assets/images/logodemo.png'),
-                const SizedBox(
-                  height: 32,
+          SizedBox(
+            width: 161.0,
+            height: 52.0,
+            child: TextButton(
+                child: Text(
+                  'Đăng việc ngay',
+                  style: FontStyle().registerFont,
                 ),
-                SizedBox(
-                  width: 161.0,
-                  height: 52.0,
-                  child: TextButton(
-                    child: Text(
-                      'Đăng việc ngay',
-                      style: FontStyle().registerFont,
-                    ),
-                    style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStateProperty.all(ColorApp.orangeColor),
-                    ),
-                    onPressed: () {},
-                  ),
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all(ColorApp.orangeColor),
                 ),
-              ],
-            ),
-          )),
-          // const Spacer(),
+                onPressed: _onAddTask),
+          ),
         ],
       ),
     );
