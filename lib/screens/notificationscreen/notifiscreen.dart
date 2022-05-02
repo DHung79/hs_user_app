aca1460f6928887ecf50c9ed1bf90e8905ab4b3b
Nguyễn Đức Hoàng Phi <nguyenduchoangphi922016@gmail.com>
2022-05-02 15:12:41 -0700

    update notifi page ui

--- lib/screens/notificationscreen/notifiscreen.dart
+++ lib/screens/notificationscreen/notifiscreen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-import '../../config/theme.dart';
 import '/config/theme.dart';
+import '/config/fonts.dart';
 
 class NotificationScreen extends StatefulWidget {
   const NotificationScreen({Key? key}) : super(key: key);
@@ -15,13 +15,18 @@ class _NotificationScreenState extends State<NotificationScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Thông báo'),
+        backgroundColor: ColorApp.textColor2,
+        title: Text(
+          'Thông báo',
+          style: FontStyle().serviceFont,
+        ),
         centerTitle: true,
-        actions: [
-          SvgPicture.asset('assets/icons/17078.svg')
-        ],
+        actions: [SvgPicture.asset('assets/icons/17078.svg')],
         leading: IconButton(
-          icon: SvgPicture.asset('assets/icons/17079.svg', width: 20,),
+          icon: SvgPicture.asset(
+            'assets/icons/17079.svg',
+            width: 20,
+          ),
           onPressed: () {
             Navigator.pop(context);
           },
@@ -30,78 +35,72 @@ class _NotificationScreenState extends State<NotificationScreen> {
       body: ListView(
         // scrollDirection: Axis.horizontal,
         children: <Widget>[
-          Container(
-            padding: EdgeInsets.only(left: 10, top: 16, bottom: 16),
-            child: Row(
-              children: [
-                const Padding(
-                  padding: EdgeInsets.only(left: 10, right: 19),
-                  child: CircleAvatar(
-                    backgroundColor: ColorApp.purpleColor,
-                  ),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: const [
-                    Text('Tasker huy cong viec'),
-                    SizedBox(
-                      width: 300,
-                     child: Text('dsdssddssssssssssd sadssds dds ds ds ds sddsdssdds as', overflow: TextOverflow.ellipsis, maxLines: 5,),
-                    ),
-                    Text('ajijij'),
-                  ],
-                )
-              ],
+          contentNoti(),
+          contentNoti(),
+          contentNoti(),
+          contentNoti(),
+          contentNoti(),
+          contentNoti(),
+          contentNoti(),
+          contentNoti(),
+          contentNoti(),
+          contentNoti(),
+          contentNoti(),
+        ],
+      ),
+    );
+  }
+
+  Container contentNoti() {
+    return Container(
+      decoration: const BoxDecoration(
+          border: Border(
+              left: BorderSide(
+                width: 4.0,
+                color: ColorApp.orangeColor,
+              ),
+              bottom: BorderSide(
+                  color: Color.fromRGBO(243, 244, 244, 1), width: 1))),
+      padding: const EdgeInsets.only(left: 10, top: 16, bottom: 16),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Padding(
+            padding: EdgeInsets.only(left: 10, right: 19),
+            child: CircleAvatar(
+              backgroundColor: ColorApp.purpleColor,
             ),
           ),
-          Container(
-            padding: EdgeInsets.only(left: 10, top: 16, bottom: 16),
-            child: Row(
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                const Padding(
-                  padding: EdgeInsets.only(left: 10, right: 19),
-                  child: CircleAvatar(
-                    backgroundColor: ColorApp.purpleColor,
+                Text(
+                  'Tasker hủy công việc',
+                  style: FontStyle().topNavActive,
+                ),
+                const SizedBox(
+                  height: 4,
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  child: Text(
+                    'Nguyễn Phúc Vĩnh Kỳ đã hủy công việc của bạn',
+                    style: FontStyle().topNavNotActive,
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 5,
                   ),
                 ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: const [
-                    Text('Tasker huy cong viec'),
-                    SizedBox(
-                      width: 300,
-                      child: Text('dsdssddssssssssssd sadssds dds ds ds ds sddsdssdds as', overflow: TextOverflow.ellipsis, maxLines: 5,),
-                    ),
-                    Text('ajijij'),
-                  ],
-                )
-              ],
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.only(left: 10, top: 16, bottom: 16),
-            child: Row(
-              children: [
-                const Padding(
-                  padding: EdgeInsets.only(left: 10, right: 19),
-                  child: CircleAvatar(
-                    backgroundColor: ColorApp.purpleColor,
-                  ),
+                const SizedBox(
+                  height: 4,
                 ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: const [
-                    Text('Tasker huy cong viec'),
-                    SizedBox(
-                      width: 300,
-                      child: Text('dsdssddssssssssssd sadssds dds ds ds dssssssssssssssssssssd sdddddddddddddddddddd dsssssssssssssssssss sddsdssds qsas sds sddsdssdds as', overflow: TextOverflow.ellipsis, maxLines: 5,),
-                    ),
-                    Text('ajijij'),
-                  ],
-                )
+                Text(
+                  'Vừa mới',
+                  style: FontStyle().timeDeleteTask,
+                ),
               ],
             ),
-          ),
+          )
         ],
       ),
     );
