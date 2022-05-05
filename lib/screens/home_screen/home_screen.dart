77079cc7b95cee58b99f645ec61b34216671aa5e
Hùng97 <daoleduyhung04@gmail.com>
2022-05-05 13:18:20 +0700

    fix conflicts

--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -1,23 +1,38 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import '/screens/otp_screen/otp_screen.dart';
-import '../login_screen/login_screen.dart';
+import 'package:hs_user_app/screens/home_screen/components/home_navigation.dart';
 
-class HomeScreen extends StatelessWidget {
+import 'booking/booking.dart';
+import 'components/home_content.dart';
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+  final List<Widget> _widgetOptions = <Widget>[
+    const HomeContent(),
+    const Booking(),
+    const HomeContent(),
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: StreamBuilder(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return const OtpScreen();
-          } else {
-            return const LoginScreen();
-          }
-        },
+    return SafeArea(
+      child: GestureDetector(
+        child: Scaffold(
+            body: _widgetOptions.elementAt(_selectedIndex),
+            bottomNavigationBar: HomeNavigation(
+              currentIndex: _selectedIndex,
+              onTap: (index) {
+                setState(() {
+                  _selectedIndex = index;
+                });
+              },
+            )),
       ),
     );
   }
